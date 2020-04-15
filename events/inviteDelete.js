9bd668cf2638f6ae0f3450fb4f00a65d19015f46
SapphireVen <63673693+SapphireVen@users.noreply.github.com>
2020-04-15 10:29:12 -0400

    Update inviteDelete.js

--- events/inviteDelete.js
+++ events/inviteDelete.js
@@ -1,22 +1,20 @@
 const Discord = require('discord.js');
-const moment = require('moment');
-const today = moment(new Date());
 
 module.exports = (invite) => {
     const invitedeletedocolor = "#f70303"
 
     // ─── DEFINED GUILDS ─────────────────────────────────────────────────────────────────────
-    const gayzone = invite.guilds.cache.get("699015211096473670");
+    const gayzone = invite.guilds.cache.get("GUILD ID");
     // ─────────────────────────────────────────────────────────────────────────────────
 
     // ─── GUILD LOGS ─────────────────────────────────────────────────────────────────────
-    const gayzonelogs = invite.channels.cache.get("699324175965356184");
+    const gayzonelogs = invite.channels.cache.get("CHANNEL ID");
     // ─────────────────────────────────────────────────────────────────────────────────
 
 
     const invitedelete = new Discord.MessageEmbed()
         .setColor(invitedeletedocolor)
-        .setDescription(`Invite was deleted at ${today.format('**LT**')}.`)
+        .setDescription(`Invite was deleted.`)
 
 
     // ─── GUILDS ─────────────────────────────────────────────────────────────────────
