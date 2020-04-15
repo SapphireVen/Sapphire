8c8b9dbc466ebdacad719f0fd08c7ead175e222d
SapphireVen <63673693+SapphireVen@users.noreply.github.com>
2020-04-15 10:27:07 -0400

    Update roleCreate.js

--- events/roleCreate.js
+++ events/roleCreate.js
@@ -4,11 +4,11 @@ module.exports = (client, role) => {
     const rolecreatecolor = "#000000"
 
     // ─── DEFINED GUILDS ─────────────────────────────────────────────────────────────────────
-    const gayzone = client.guilds.cache.get("699015211096473670");
+    const gayzone = client.guilds.cache.get("GUILD ID");
     // ─────────────────────────────────────────────────────────────────────────────────
 
     // ─── GUILD LOGS ─────────────────────────────────────────────────────────────────────
-    const gayzonelogs = client.channels.cache.get("699324175965356184");
+    const gayzonelogs = client.channels.cache.get("CHANNEL ID");
     // ─────────────────────────────────────────────────────────────────────────────────
 
 
